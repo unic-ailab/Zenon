b3cfe4882024f7f78483ec6efdbe366e98924b37
cchloee <chloe.chira@fountech.ai>
2022-08-01 14:32:35 +0300

    New rest channel which accepts metadata

---
+++ custom_rest_channel.py
@@ -0,0 +1,18 @@
+from rasa.core.channels.rest import RestInput
+from sanic.request import Request
+from typing import Text, Dict, Any, Optional, Callable, Awaitable, NoReturn, Union
+
+
+class RestMetadataInput(RestInput):
+    def name(name) -> Text:
+        """Name of your custom channel."""
+        return "rest_metadata"
+
+    def get_metadata(self, req: Request) -> Optional[Dict[Text, Any]]:
+        """Extracts the metadata from a user message.
+        Args:
+            request: A `Request` object
+        Returns:
+            Metadata extracted from the sent event payload.
+        """
+        return req.json.get("metadata", None)
